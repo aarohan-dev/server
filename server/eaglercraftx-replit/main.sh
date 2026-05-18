9ac14737fd93fb8213958b49bc9cb4aae1c39cea
aarohan-dev <aarohanskarki@gmail.com>
2026-05-18 06:09:02 -0400

    Refactor main.sh to streamline server startup process

--- server/eaglercraftx-replit/main.sh
+++ server/eaglercraftx-replit/main.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
-
 # 1. Accept EULA
 mkdir -p server
 echo "eula=true" > server/eula.txt
 
-# 2. Grant permissions
-chmod +x bungee/bungee.jar
-chmod +x server/server.jar
-
-# 3. Start the Bungee Proxy with SQLite driver attached
-# We use -cp to include the sqlite jar so it doesn't try to download it
+# 2. Start the Proxy and FORCIBLY ignore the download requirement
 echo "Starting Bungee Proxy..."
 cd bungee
-java -Xmx256M -cp "bungee.jar:sqlite-jdbc-3.45.0.0.jar" net.md_5.bungee.Bootstrap & 
+# We add the STFU flag to hide warnings and ensure it uses local resources
+java -Xmx256M -Deaglerxbungee.stfu=true -jar bungee.jar & 
 
-# 4. Give bungee more time to initialize (Render can be slow)
-sleep 45
+# 3. Wait for the Gateway to settle
+sleep 30
 
-# 5. Start the Minecraft Server
+# 4. Start the Minecraft Server
 echo "Starting Minecraft Server..."
 cd ../server
-java -Xmx512M -jar server.jar nogu
+java -Xmx512M -jar server.jar nogui
